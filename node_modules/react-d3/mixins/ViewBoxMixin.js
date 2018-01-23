4dbabb9fba8b4d4b04313076cc8d1af638374fbe
mayakoneval <mayakoneval@gmail.com>
2018-01-23 11:31:49 -0500

    including the react-d3 module because I am editing it heavily, to make it run andto make it work for my purposes

---
+++ node_modules/react-d3/mixins/ViewBoxMixin.js
@@ -0,0 +1,37 @@
+
+'use strict';
+
+var React = require('react');
+var PropTypes = require('prop-types');
+
+module.exports =  {
+
+  propTypes: {
+    viewBox:           PropTypes.string,
+    viewBoxObject:     PropTypes.object
+  },
+
+  getViewBox:function() {
+    if (this.props.viewBoxObject) {
+      var v = this.props.viewBoxObject;
+      return [v.x, v.y, v.width, v.height].join(' ');
+    } else if (this.props.viewBox) {
+      return this.props.viewBox;
+    } 
+  },
+
+  getOuterDimensions:function() {
+    if (this.props.viewBoxObject) {
+      return {
+        width: this.props.viewBoxObject.width,
+        height: this.props.viewBoxObject.height
+      };
+    } else {
+      return {
+        width: this.props.width,
+        height: this.props.height
+      };
+    }
+  }
+
+};
